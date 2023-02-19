424ed87322c6d397e0861d6ed4db391a3e8b3870
MariLFO <mari.fer.onorio@gmail.com>
2023-02-19 14:52:43 -0800

    Rota Post

--- livro-servidor/routes/livros.js
+++ livro-servidor/routes/livros.js
@@ -7,3 +7,13 @@ router.get('/', async (req, res) => {
     const livros = await LivroDAO.obterLivros();
     res.json(livros);
 });
+
+router.post('/', async (req, res) => {
+    const livro = req.body;
+    try {
+        await LivroDAO.incluir(livro);
+        res.status(201).json({ mensagem: 'Livro adicionado com sucesso.' });
+    } catch (erro) {
+        res.status(500).json({ mensagem: 'Erro ao adicionar livro.' });
+    }
+});
