7e98ebf90fcb53231b569116f0e65d1042a53fdf
MariLFO <mari.fer.onorio@gmail.com>
2023-02-19 14:44:10 -0800

    Função incluir

--- livro-servidor/modelos/livro-dao.js
+++ livro-servidor/modelos/livro-dao.js
@@ -6,3 +6,8 @@ const obterLivros = async () => {
     const livros = await Livro.find();
     return livros;
 };
+
+const incluir = async (livro) => {
+    const novoLivro = await Livro.create(livro);
+    return novoLivro;
+};
